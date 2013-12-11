c738018be082bdbc12479a306c7004ab3d440353
Han Fei <hanfei19910905@gmail.com>
2013-12-11 20:29:02 +0800

    convert jingque.cc to single.hpp

--- single.hpp
+++ single.hpp
@@ -40,7 +40,6 @@ void allocat(int base,int n){
 				int x = spj[i][j][0];
 				int y = spj[i][j][1];
 				if(x > y) swap(x,y);
-//				if((y - x) % 2 == 0) swap(x,y);
 				ANS[i].PB(make_pair(x+base,y+base));
 			}
 		return ;
@@ -52,18 +51,15 @@ void allocat(int base,int n){
 			int x = tmp[j] + base, y = tmp[n-j-1] + base;
 			if(x > y) swap(x,y);
 			if(x == base) continue;
-//			if((y - x) % 2 == 0) swap(x,y);
 			ANS[i].PB(make_pair(x,y));
 		}
 		int val = tmp.back();
 		tmp.insert(tmp.begin()+1,val);
 		tmp.erase(tmp.end()-1);
-//		for(int i = 0; i < n; i++) cout<< tmp[i]<<" ";cout<<endl;
 	}
 }
 int diff,coln,rown;
 int hash[N][N];
-//bool _hash[N][N][N];
 int _vis[N][N];
 pii ans[N][N];
 pii last_ans[N][N];
@@ -107,7 +103,6 @@ void dfs(int x,int y,int c){
 	for(int i = 1; i <= cntn; i++)
 		sum += differ(hash[i]);
 	if(sum > TMP+EPS) return;
-//	if(sum > best) return ;
 	if(y == ANS[x].size()) x ++, y = 0;
 	if(x == len) {
 		if(sum == TMP) {
@@ -117,17 +112,14 @@ void dfs(int x,int y,int c){
 			for(int i = 1; i <= base; i++)
 				if(chk_zero(hash[i]) > 1)
 					return ;
-//			cout<<c<<endl;
 			best = sum;
 			memcpy(last_ans,ans,sizeof(ans));
 			memcpy(myconf,conf,sizeof(conf));
 		}
 		return;
 	}
-//	cout<<"x,y: "<<x<<" "<<y<<endl;
 	pii p = ANS[x][y];
 	int a =p.first, b = p.second;
-//	cout<<"a,b: "<<a<<" "<<b<<endl;
 	for(int i =  0; i < coln; i++)
 		temp[i] = make_pair(use[i],i);
 	sort(temp,temp+coln);
@@ -143,25 +135,11 @@ void dfs(int x,int y,int c){
 				if(coln < 3 || i == 0 || !conflict(ans[i-1][j],a,b))
 					
 					{
-					/*
-					for(int row = i - 1; row >= 0; row --) if(ans[row][j].first == a){
-						cost ++;
-						break;
-					} else if(ans[row][j].second == a) break;
-					
-					for(int row = i - 1; row >= 0; row --) if(ans[row][j].second == b){
-						cost ++;
-						break;
-					} else if(ans[row][j].first == a) break;
-					if(cost + conf[j] > lim ) continue;
-					*/
 					_vis[a][i] = 1;
 					_vis[b][i] = 1;
 					ans[i][j] = p;
-				//	conf[j] += cost;
 					dfs(x,y+1,c+1);
 					if(flag) return ;
-				//	conf[j] -= cost;
 					_vis[a][i] = 0;
 					_vis[b][i] = 0;
 					ans[i][j] = zero;
@@ -171,34 +149,26 @@ void dfs(int x,int y,int c){
 		hash[a][j]--;
 		hash[b][j]--;
 	}
-//	cout<<x<<" "<<y<<" fail\n";
 }
 int color[105][105],lastc[105];
-int main(){
-//    freopen("out.txt","w",stdout);
+int Solve(char* __ans,int coln,vector<int> input){
 	int tst,t = 0,myn = 0;
 	base = 0;
-	cin >> tst >> coln;
+    int tst = input.size();
 	for(int i = 0; i < tst; i++){
-		int value;
-		cin >> value;
-		cout<< value<<" ";
+		int value = input[i];
 		myn += value/2;
 		allocat(base,value);
 		base += value;
 	}
-	cout<<endl;
 	cntn = base;
 	int sum = 0;
 	for(int i = 0; i < len; i++) sum += ANS[i].size();
 	tmpn = min(coln,myn);
 	rown = sum / tmpn + !!(sum % tmpn);
 	TMP = !!((cntn-1) % coln) * rown;
-//	cout<<"TMP: "<<TMP<<endl;
 	bool myflag = 0;
 	for( EPS = 2;; EPS++){
-//		for( lim = 0; lim <= 10; lim ++){
-//			cout<<EPS<<" "<<lim<<endl;
 			memset(conf,0,sizeof(conf));
 			flag = 0;
 			init();
@@ -206,19 +176,15 @@ int main(){
 			dfs(0,0,0);
 			if(myflag) break;
 			if(best != 10000) {
-				//cout<<best<<" "<<TMP<<endl;
 				if(flag || best == TMP) break;
 				else myflag = 1;
 		};
-//	}
-//	if(myflag || flag || best == TMP) break;
 	}
-//	cout<<"BEST: "<<best<<endl;
 	for(int i = 0; i < rown; i++){
 		for(int j = 0; j < coln; j++){
-			printf("%d,%d ",last_ans[i][j].first,last_ans[i][j].second);
+			sprintf(__ans,"%d,%d ",last_ans[i][j].first,last_ans[i][j].second);
 		}
-		cout<< endl;
+        sprintf(__ans,"\n");
 	}
-//	for(int i = 0; i < coln; i++) cout<< myconf[i]<<" ";cout<<endl;
+    return 1;
 }
