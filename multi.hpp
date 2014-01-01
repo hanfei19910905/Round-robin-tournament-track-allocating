d7840cc6aa8c8dbfef7b58793269a02ec5468163
figo <hanfei19910905@gmail.com>
2014-01-01 17:15:57 +0800

    add some comments

--- multi.hpp
+++ multi.hpp
@@ -12,169 +12,174 @@ using namespace std;
 #define ALL(a) a.begin(),a.end()
 #define PB(a) push_back(a)
 namespace MULTI{
-typedef pair<int,int> pii;
-int val[100],N,vis[100];
-// 队伍数量为8的特殊考虑，这是一个三维数组
-int spj[8][4][2] = {
-    {{1,2},{3,4},{5,6},{7,8}},
-    {{5,3},{1,7},{2,8},{6,4}},
-    {{1,4},{6,2},{7,3},{5,8}},
-    {{2,7},{1,8},{4,5},{3,6}},
-    {{4,8},{6,7},{2,3},{1,5}},
-    {{5,7},{3,8},{1,6},{2,4}},
-    {{6,8},{2,5},{4,7},{1,3}}
-};
-vector<pii> ANS[105];
-void allocat(int base,int n){
-    // 这部分逻辑和单组的allocat函数是一样的，用固定轮转法生成一个“估计结果”，存放在ANS数组中。
-    if(n == 8) {
-        for(int i = 0; i < n-1; i++)
+    typedef pair<int,int> pii;
+    // 队伍数量为8的特殊考虑，这是一个三维数组
+    int spj[8][4][2] = {
+        {{1,2},{3,4},{5,6},{7,8}},
+        {{5,3},{1,7},{2,8},{6,4}},
+        {{1,4},{6,2},{7,3},{5,8}},
+        {{2,7},{1,8},{4,5},{3,6}},
+        {{4,8},{6,7},{2,3},{1,5}},
+        {{5,7},{3,8},{1,6},{2,4}},
+        {{6,8},{2,5},{4,7},{1,3}}
+    };
+    vector<pii> ANS[105];
+    // base是已经确定了多少个队伍，n是当前组的队伍数
+    void allocat(int base,int n){
+        if(n == 8) {
+            for(int i = 0; i < n-1; i++)
+                for(int j = 0; j < n/2; j++){
+                    int x = spj[i][j][0];
+                    int y = spj[i][j][1];
+                    if(x > y) swap(x,y);
+                    if((y - x) % 2 == 0) swap(x,y);
+                    ANS[i].PB(make_pair(x+base,y+base));
+                }
+            return ;
+        }
+        // 固定轮转法
+        vector<int> tmp;
+        for(int i = 1; i<=n; i++) tmp.PB(i);
+        if(n&1) {tmp.PB(0);n++;}
+        for(int i =0 ; i <n-1;i++){
             for(int j = 0; j < n/2; j++){
-                int x = spj[i][j][0];
-                int y = spj[i][j][1];
+                int x = tmp[j] + base, y = tmp[n-j-1] + base;
                 if(x > y) swap(x,y);
+                if(x == base) continue;
                 if((y - x) % 2 == 0) swap(x,y);
-                ANS[i].PB(make_pair(x+base,y+base));
+                ANS[i].PB(make_pair(x,y));
             }
-        return ;
-    }
-    vector<int> tmp;
-    for(int i = 1; i<=n; i++) tmp.PB(i);
-    if(n&1) {tmp.PB(0);n++;}
-    for(int i =0 ; i <n-1;i++){
-        for(int j = 0; j < n/2; j++){
-            int x = tmp[j] + base, y = tmp[n-j-1] + base;
-            if(x > y) swap(x,y);
-            if(x == base) continue;
-            if((y - x) % 2 == 0) swap(x,y);
-            ANS[i].PB(make_pair(x,y));
+            int val = tmp.back();
+            tmp.insert(tmp.begin()+1,val);
+            tmp.erase(tmp.end()-1);
         }
-        int val = tmp.back();
-        tmp.insert(tmp.begin()+1,val);
-        tmp.erase(tmp.end()-1);
     }
-}
-pii ans[10005][105];
-const pii ZERO = make_pair(0,0);
-int hash[105][105],now,col,SUM[105];
-double dif[105];
-bool cmp1(pii a,pii b){
-    return (dif[a.first] + dif[a.second]) * (SUM[b.first] + SUM[b.second]) > (dif[b.first] + dif[b.second]) * (SUM[a.first] + SUM[a.second]);
-}
-// cal函数的作用是计算hash数组的方差
-double cal(int hash[],int k){
-    double sum = 0,val = 0;
-    for(int j = 0; j < k; j++)
-        sum += hash[j];
-    sum /= k;
-    for(int j = 0; j < k; j++)
-        val += pow(abs(hash[j] - sum),2);
-    return val;
-}
-// diff函数的作用是计算分布率最大最小值插叙。
-int diff(int hash[],int n){
-    int mx = 0,mn = 1000000000;
-    for(int i = 0; i < n;i++){
-        if(hash[i] > mx) mx = hash[i];
-        if(mn > hash[i]) mn = hash[i];
-    } return mx - mn;
-}
-int last[105],row;
-double factor = 0.3;
-map<int,int> up[100];
-//为某个队伍在论次col贪心选择一个赛道
-int MAX(int col,int k,int n,bool flag){
-    for(int i = 1; i <= n; i++){
-        dif[i] = cal(hash[i],k);
+    // ans是最终结果
+    pii ans[10005][105];
+    const pii ZERO = make_pair(0,0);
+    // hash为分布率
+    int hash[105][105],SUM[105];
+    double dif[105];
+    // 按照 方差/出现次数 排序
+    bool cmp1(pii a,pii b){
+        return (dif[a.first] + dif[a.second]) * (SUM[b.first] + SUM[b.second]) > 
+               (dif[b.first] + dif[b.second]) * (SUM[a.first] + SUM[a.second]);
     }
-    if(ANS[row].size() == 0) row ++;
-    vector<pii>& t = ANS[row];
-    if(t.size()==0) return -2;
-    sort(ALL(t),cmp1);
-    vector<pii> :: iterator tmp,it=t.begin();
-    int S=-1;
-    for(;;it++){
-        pii x = ZERO;
-        for(; it != t.end(); it++){
-            pii v = *it;
-            if(!vis[v.first] && !vis[v.second]){
-                x = v;
-                tmp = it;
-                break;
+    // cal函数的作用是计算hash数组的方差
+    double cal(int hash[],int k){
+        double sum = 0,val = 0;
+        for(int j = 0; j < k; j++)
+            sum += hash[j];
+        sum /= k;
+        for(int j = 0; j < k; j++)
+            val += pow(abs(hash[j] - sum),2);
+        return val;
+    }
+    // diff函数的作用是计算分布率最大最小值之差。
+    int diff(int hash[],int n){
+        int mx = 0,mn = 1000000000;
+        for(int i = 0; i < n;i++){
+            if(hash[i] > mx) mx = hash[i];
+            if(mn > hash[i]) mn = hash[i];
+        } return mx - mn;
+    }
+    // row表示当前处理到了哪个轮次,last[i]代表队伍i上次在哪个赛道出现。
+    int last[105],row;
+    // vis代表当前轮次中是否被访问过
+    int vis[100];
+    //为某个队伍在论次col贪心选择一个赛道
+    // flag是，按照“均匀分布”的策略是否能找到方案的一个标记
+    // k是赛道数，n是队伍总数
+    int MAX(int col,int k,int n,bool flag){
+        for(int i = 1; i <= n; i++){
+            dif[i] = cal(hash[i],k);
+        }
+        if(ANS[row].size() == 0) row ++;
+        // t是当前轮次数组
+        vector<pii>& t = ANS[row];
+        if(t.size()==0) return -2;
+        sort(ALL(t),cmp1);
+        vector<pii> :: iterator tmp,it=t.begin();
+        for(;;it++){
+            // x是确定要选择的比赛
+            pii x = ZERO;
+            // 选择一场合法的比赛
+            for(; it != t.end(); it++){
+                pii v = *it;
+                if(!vis[v.first] && !vis[v.second]){
+                    x = v;
+                    tmp = it;
+                    break;
+                }
+            }
+            if(x == ZERO) return -1;
+            // s是为x分配的赛道
+            int a = x.first, b= x.second,s = -1;double mx = 1e20;
+            for(int i = 0; i < k; i++) if(ans[col][i] == ZERO) {
+                if((i == last[a] || i == last[b]) && k > 2) continue;
+                hash[a][i]++;
+                hash[b][i]++;
+                double val = cal(hash[a],k) + cal(hash[b],k);
+                if(diff(hash[a],k)< 10 || diff(hash[b],k)<10 || !flag)
+                    if( val < mx){mx = val; s = i;}
+                hash[a][i]--;
+                hash[b][i]--;
+            }
+            if(s!=-1){
+                ans[col][s] = x;
+                hash[a][s] ++;
+                hash[b][s] ++;
+                SUM[a]++;
+                SUM[b]++;
+                last[a] = last[b] = s;
+                t.erase(tmp);
+                vis[a] = 1; vis[b] = 1;
+                return 0;
             }
         }
-        // x是确定要选择的比赛
-        if(x == ZERO) return -1;
-        int a = x.first, b= x.second,s = -1;double mx = 1e20;
-        for(int i = 0; i < k; i++) if(ans[col][i] == ZERO) {
-            if((i == last[a] || i == last[b]) && k > 2) continue;
-            hash[a][i]++;
-            hash[b][i]++;
-            double val = cal(hash[a],k) + cal(hash[b],k);
-            if(up[i][a] == 1) val += factor;
-            if(up[i][b] == 2) val += factor;
-            if(diff(hash[a],k)< 10 || diff(hash[b],k)<10 || !flag)
-                if( val < mx){mx = val; s = i;}
-            hash[a][i]--;
-            hash[b][i]--;
-        }
-        // s是为x分配的赛道
-        if(s!=-1){
-            ans[col][s] = x;
-            hash[a][s] ++;
-            hash[b][s] ++;
-            SUM[a]++;
-            SUM[b]++;
-            last[a] = last[b] = s;
-            t.erase(tmp);
-            vis[a] = 1; vis[b] = 1;
-            up[s][a] = 1;
-            up[s][b] = 2;
-            return 0;
+        return -1;
+    }
+    // col代表选到了第几个论次
+    int col = 0;
+    // k是赛道数，n是队伍数
+    void solve(int k,int n){
+        for(int i = 1; i <= n; i++)
+            last[i] = k;
+        // flag是，按照“均匀分布”的策略是否能找到方案的一个标记
+        bool flag = 1;
+        for(int _=0;_<105;_++){
+            memset(vis,0,sizeof(vis));
+            for(int i = 0; i < k; i++)
+                ans[col][i] = ZERO;
+            int a;
+            for(int i=0; i < k; i++){
+                 a = MAX(col,k,n,flag);
+                if(a == -1 || a == -2) break;
+            }
+            bool tmp = 0;
+            for(int i = 0; i < k; i++){
+                pii x = ans[col][i];
+                if(x!= ZERO) tmp = 1;
+            }
+            if(tmp) col ++;
+            else {flag = 0;}
+            if(a == -2) break;
         }
     }
-    return -1;
-}
-void solve(int k,int n){
-    now = 0, col = 0;
-    for(int i = 1; i <= n; i++)
-        last[i] = k;
-    bool flag = 1;
-    // ans是最终结果，col代表选到了第几个论次
-    for(int _=0;_<105;_++){
-        memset(vis,0,sizeof(vis));
-        for(int i = 0; i < k; i++)
-            ans[col][i] = ZERO;
-        int a;
-        for(int i=0; i < k; i++){
-             a = MAX(col,k,n,flag);
-            if(a == -1 || a == -2) break;
+    int Solve(vector<vector<pii> >& __ans,int m,vector<int> input){
+        int tst = input.size(),t = 0,N= 0;
+        for(int i = 0; i < tst; i++){
+            int value = input[i];
+            allocat(N,value);
+            N += value;
         }
-        bool tmp = 0;
-        for(int i = 0; i < k; i++){
-            pii x = ans[col][i];
-            if(x!= ZERO) tmp = 1;
+        solve(m,N);
+        __ans.resize(col);
+        for(int i = 0; i < col; i++){
+            for(int j = 0; j < m; j++){
+                __ans[i].push_back(ans[i][j]);
+            }
         }
-        if(tmp) col ++;
-        else {flag = 0;}
-        if(a == -2) break;
+        return 1;
     }
-}
-
-int Solve(vector<vector<pii> >& __ans,int m,vector<int> input){
-    int tst = input.size(),t = 0;N= 0;
-    for(int i = 0; i < tst; i++){
-        int value = input[i];
-        allocat(N,value);
-        N += value;
-    }
-    solve(m,N);
-    __ans.resize(col);
-    for(int i = 0; i < col; i++){
-        for(int j = 0; j < m; j++){
-            __ans[i].push_back(ans[i][j]);
-        }
-    }
-    return 1;
-}
 };
--- single.hpp
+++ single.hpp
@@ -7,214 +7,210 @@
 #include<algorithm>
 using namespace std;
 namespace SINGLE{
-#define ALL(a) a.begin(),a.end()
-#define PB(a) push_back(a)
-typedef pair<int,int> pii;
-const int N = 105;
-const pii zero = make_pair(0,0);
-int val[N],vis[N];
-vector<pii> ANS[N];
-int len = 0;
-int spj[8][4][2] = {
-    {{1,2},{3,4},{5,6},{7,8}},
-    {{5,3},{1,7},{2,8},{6,4}},
-    {{1,4},{6,2},{7,3},{5,8}},
-    {{2,7},{1,8},{4,5},{3,6}},
-    {{4,8},{6,7},{2,3},{1,5}},
-    {{5,7},{3,8},{1,6},{2,4}},
-    {{6,8},{2,5},{4,7},{1,3}}
-};
-bool tmp[105], odd[105];
-void allocat(int base,int n){
-    // 使用固定轮转法确定比赛，将结果存放在ANS中。
-    for(int i = 1; i <= n; i++){
-        tmp[i + base] = n&1;
-        odd[i + base] = i&1;
-    }
-    vector<int> tmp;
-    for(int i = 1; i<=n; i++) tmp.PB(i);
-    int maxn = n&1 ? n : n-1;
-    if(maxn > len) len = maxn;
-
-    if(n == 8) {
-        for(int i = 0; i < n-1; i++)
-            for(int j = 0; j < n/2; j++){
-                int x = spj[i][j][0];
-                int y = spj[i][j][1];
-                if(x > y) swap(x,y);
-                ANS[i].PB(make_pair(x+base,y+base));
-            }
-        return ;
-    }
-    
-    if(n&1) {tmp.PB(0);n++;}
-    for(int i =0 ; i <n-1;i++){
-        for(int j = 0; j < n/2; j++){
-            int x = tmp[j] + base, y = tmp[n-j-1] + base;
-            if(x > y) swap(x,y);
-            if(x == base) continue;
-            ANS[i].PB(make_pair(x,y));
-        }
-        int val = tmp.back();
-        tmp.insert(tmp.begin()+1,val);
-        tmp.erase(tmp.end()-1);
-    }
-}
-int diff,coln,rown;
-int hash[N][N];
-int _vis[N][N];
-pii ans[N][N];
-pii last_ans[N][N];
-int last[N],use[N];
-// 初始化一些全局变量
-bool init(){
-    memset(last,-1,sizeof(last));
-    memset(hash,0,sizeof(hash));
-    memset(use,0,sizeof(use));
-    memset(_vis,0,sizeof(_vis));
-    for(int i = 0; i <= rown; i++)
-        for(int j = 0; j < coln; j++)
-            ans[i][j] = zero;
-    return 1;
-}
-// conflict数组计算比赛x(即x.first,x.second)和比赛(a,b)是否能排在相邻两轮
-bool conflict(pii x,int a,int b){
-    if(x == zero) return 0;
-    int c = x.first, d = x.second;
-    return a == c || a == d || c == b || b == d;
-}
-// differ 计算某个队伍的hash（分布律）数组的最大最小值的差
-int differ(int hash[N]){
-    int mx = -1, mn = coln+1;
-    for(int i = 0; i < coln; i++){
-        if(mn > hash[i]) mn = hash[i];
-        if(mx < hash[i]) mx = hash[i];
-    }
-    return mx - mn;
-}
-// chk_zero counts all the zeros in array hash
-int chk_zero(int hash[N]){
-    int ans = 0;
-    for(int i = 0; i < coln; i++)
-        ans += hash[i] == 0;
-    return ans;
-}
-int cntn,tmpn,TMP,best,EPS,flag,lim,base;
-int conf[100] ,myconf[100];
-bool tight_col;
-// dfs 计算最终结果并储存在last_ans中
-// 将分布律储存在hash中
-// 全局变量flag = 1说明找到了最理想结果
-// len是比赛预期的轮数
-// 参数x,y代表计算到了比赛ANS[x][y]，在当前dfs中，需要为它安排一个赛道。
-// c是已经安排了几场比赛
-void dfs(int x,int y,int c){
-    // 如果找到了最理想结果，则不必继续搜索
-    if(flag) return ;
-    pii temp[N];
-    // expt表示比赛(x,y)预计要安排到的轮次数
-    int expt = c / tmpn;
-    int sum = 0;
-    for(int i = 1; i <= cntn; i++)
-        sum += differ(hash[i]);
-    // sum表示当前的方差和
-    if(sum > TMP+EPS) return;
-    // 此处是一个剪枝，如果继续搜索没有当前结果好，则舍弃
-    if(y == ANS[x].size()) x ++, y = 0;
-    if(x == len) {
-        // 搜索到终点
-        if(sum == TMP) {
-            flag = 1;
-        }
-        // 如果当前结果比最优结果好，那么记录当前结果
-        if(sum < best ) {
-            // 按照之前的要求，某行的轮空不超过1
-            for(int i = 1; i <= base; i++)
-                if(chk_zero(hash[i]) > 1)
-                    return ;
-            best = sum;
-            memcpy(last_ans,ans,sizeof(ans));
-            memcpy(myconf,conf,sizeof(conf));
-        }
-        return;
-    }
-    pii p = ANS[x][y];
-    int a =p.first, b = p.second;
-    for(int i =  0; i < coln; i++)
-        temp[i] = make_pair(use[i],i);
-    // 数组use是赛道的使用频率
-    sort(temp,temp+coln);
-    // 先从使用频率最小的赛道开始检查
-    for(int  q = 0; q < coln; q++){
-        int j = temp[q].second;
-        hash[a][j]++;
-        hash[b][j]++;
-        use[j] ++;
-        // 假设选择赛道j，更新hash && use 数组
-        int dif = coln <= 4 ? 3 : 2;
-        if(differ(hash[a]) <= dif && differ(hash[b]) <= dif) {
-            int i = expt,cost = 0;
-            if(ans[i][j] == zero && !_vis[a][i] && !_vis[b][i]) 
-                // tight_col是代表列数 <=2
-                if(tight_col || i == 0 || !conflict(ans[i-1][j],a,b)){
-                    _vis[a][i] = 1;
-                    _vis[b][i] = 1;
-                    ans[i][j] = p;
-                    dfs(x,y+1,c+1);
-                    if(flag) return ;
-                    _vis[a][i] = 0;
-                    _vis[b][i] = 0;
-                    ans[i][j] = zero;
+    #define ALL(a) a.begin(),a.end()
+    #define PB(a) push_back(a)
+    typedef pair<int,int> pii;
+    const int N = 105;
+    const pii zero = make_pair(0,0);
+    vector<pii> ANS[N];
+    int len = 0;
+    // len是ANS的size
+    int spj[8][4][2] = {
+        {{1,2},{3,4},{5,6},{7,8}},
+        {{5,3},{1,7},{2,8},{6,4}},
+        {{1,4},{6,2},{7,3},{5,8}},
+        {{2,7},{1,8},{4,5},{3,6}},
+        {{4,8},{6,7},{2,3},{1,5}},
+        {{5,7},{3,8},{1,6},{2,4}},
+        {{6,8},{2,5},{4,7},{1,3}}
+    };
+    // base是已经确定了多少个队伍，n是当前组的队伍数
+    void allocat(int base,int n){
+        if(n == 8) {
+            for(int i = 0; i < n-1; i++)
+                for(int j = 0; j < n/2; j++){
+                    int x = spj[i][j][0];
+                    int y = spj[i][j][1];
+                    if(x > y) swap(x,y);
+                    ANS[i].PB(make_pair(x+base,y+base));
                 }
+            return ;
         }
-        // 搜索结束，回溯
-        use[j]--;
-        hash[a][j]--;
-        hash[b][j]--;
-    }
-}
-int color[105][105],lastc[105];
-int Solve(vector<vector<pii> >& __ans,int loc_coln,vector<int> input){
-    coln = loc_coln;
-    int t = 0,myn = 0;
-    base = 0;
-    int tst = input.size();
-    for(int i = 0; i < tst; i++){
-        int value = input[i];
-        myn += value/2;
-        // 使用固定轮转法确定比赛
-        allocat(base,value);
-        base += value;
-    }
-    tight_col = 0;
-    if(coln < 3) tight_col = 1;
-    if(coln == 3 && base == 8) tight_col = 1;
-    cntn = base;
-    int sum = 0;
-    for(int i = 0; i < len; i++) sum += ANS[i].size();
-    tmpn = min(coln,myn);
-    rown = sum / tmpn + !!(sum % tmpn);
-    TMP = !!((cntn-1) % coln) * rown;
-    bool myflag = 0;
-    for( EPS = 2;; EPS++){
-        // 逐步放宽EPS值的限制
-        memset(conf,0,sizeof(conf));
-        flag = 0;
-        init();
-        if(!myflag) best = 10000;
-        dfs(0,0,0);
-        if(myflag) break;
-        if(best != 10000) {
-            if(flag || best == TMP) break;
-            else myflag = 1;
+        // 使用固定轮转法确定比赛，将结果存放在ANS中。
+        vector<int> tmp;
+        for(int i = 1; i<=n; i++) tmp.PB(i);
+        int maxn = n&1 ? n : n-1;
+        if(maxn > len) len = maxn;
+        if(n&1) {tmp.PB(0);n++;}
+        for(int i =0 ; i <n-1;i++){
+            for(int j = 0; j < n/2; j++){
+                int x = tmp[j] + base, y = tmp[n-j-1] + base;
+                if(x > y) swap(x,y);
+                if(x == base) continue;
+                ANS[i].PB(make_pair(x,y));
+            }
+            int val = tmp.back();
+            tmp.insert(tmp.begin()+1,val);
+            tmp.erase(tmp.end()-1);
         }
     }
-    __ans.resize(rown);
-    for(int i = 0; i < rown; i++){
-        for(int j = 0; j < coln; j++){
-            __ans[i].push_back(last_ans[i][j]);
+    // coln是赛道数，rown是轮次数
+    int coln,rown;
+    // hash代表分布率
+    int hash[N][N];
+    // _vis[i][j]代表在轮次i队伍j是否已经比过赛，use[i]代表赛道i比了几次赛
+    int _vis[N][N],use[N];
+    // ans代表当前最优结果，last_ans代表最终的最优结果
+    pii ans[N][N], last_ans[N][N];
+    // 初始化一些全局变量
+    bool init(){
+        memset(hash,0,sizeof(hash));
+        memset(use,0,sizeof(use));
+        memset(_vis,0,sizeof(_vis));
+        for(int i = 0; i <= rown; i++)
+            for(int j = 0; j < coln; j++)
+                ans[i][j] = zero;
+        return 1;
+    }
+    // conflict数组计算比赛x(即x.first,x.second)和比赛(a,b)是否能排在相邻两轮
+    bool conflict(pii x,int a,int b){
+        if(x == zero) return 0;
+        int c = x.first, d = x.second;
+        return a == c || a == d || c == b || b == d;
+    }
+    // differ 计算某个队伍的hash（分布律）数组的最大最小值的差
+    int differ(int hash[N]){
+        int mx = -1, mn = coln+1;
+        for(int i = 0; i < coln; i++){
+            if(mn > hash[i]) mn = hash[i];
+            if(mx < hash[i]) mx = hash[i];
+        }
+        return mx - mn;
+    }
+    // chk_zero counts all the zeros in array hash
+    int chk_zero(int hash[N]){
+        int ans = 0;
+        for(int i = 0; i < coln; i++)
+            ans += hash[i] == 0;
+        return ans;
+    }
+    // 全局变量flag = 1说明找到了最理想结果,cntn是总队伍数
+    int cntn,tmpn,TMP,best,EPS,flag,base;
+    int conf[100] ,myconf[100];
+    // dfs 计算最终结果并储存在last_ans中
+    // 将分布律储存在hash中
+    // 参数x,y代表计算到了比赛ANS[x][y]，在当前dfs中，需要为它安排一个赛道。
+    // c是已经安排了几场比赛
+    // 如果赛道<=2，tight_col为true
+    bool tight_col;
+    void dfs(int x,int y,int c){
+        // 如果找到了最理想结果，则不必继续搜索
+        if(flag) return ;
+        pii temp[N];
+        // expt表示比赛(x,y)预计要安排到的轮次数
+        int expt = c / tmpn;
+        // sum表示当前的方差和
+        int sum = 0;
+        for(int i = 1; i <= cntn; i++)
+            sum += differ(hash[i]);
+        if(sum > TMP+EPS) return;
+        // 此处是一个剪枝，如果继续搜索没有当前结果好，则舍弃
+        if(y == ANS[x].size()) x ++, y = 0;
+        if(x == len) {
+            // 搜索到终点
+            if(sum == TMP) {
+                flag = 1;
+            }
+            // 如果当前结果比最优结果好，那么记录当前结果
+            if(sum < best ) {
+                // 按照之前的要求，某行的轮空不超过1
+                for(int i = 1; i <= base; i++)
+                    if(chk_zero(hash[i]) > 1)
+                        return ;
+                best = sum;
+                memcpy(last_ans,ans,sizeof(ans));
+                memcpy(myconf,conf,sizeof(conf));
+            }
+            return;
+        }
+        //p是当前比赛
+        pii p = ANS[x][y];
+        int a =p.first, b = p.second;
+        for(int i =  0; i < coln; i++)
+            temp[i] = make_pair(use[i],i);
+        // 数组use是赛道的使用频率
+        sort(temp,temp+coln);
+        // 先从使用频率最小的赛道开始检查
+        for(int  q = 0; q < coln; q++){
+            int j = temp[q].second;
+            hash[a][j]++;
+            hash[b][j]++;
+            use[j] ++;
+            // 假设选择赛道j，更新hash && use 数组
+            // dif是能容忍的hash值最大的最大最小值的差
+            int dif = coln <= 4 ? 3 : 2;
+            if(differ(hash[a]) <= dif && differ(hash[b]) <= dif) {
+                int i = expt,cost = 0;
+                if(ans[i][j] == zero && !_vis[a][i] && !_vis[b][i]) 
+                    // tight_col是代表列数 <=2
+                    if(tight_col || i == 0 || !conflict(ans[i-1][j],a,b)){
+                        _vis[a][i] = 1;
+                        _vis[b][i] = 1;
+                        ans[i][j] = p;
+                        dfs(x,y+1,c+1);
+                        if(flag) return ;
+                        _vis[a][i] = 0;
+                        _vis[b][i] = 0;
+                        ans[i][j] = zero;
+                    }
+            }
+            // 搜索结束，回溯
+            use[j]--;
+            hash[a][j]--;
+            hash[b][j]--;
         }
     }
-    return 1;
-}
+    int Solve(vector<vector<pii> >& __ans,int loc_coln,vector<int> input){
+        coln = loc_coln;
+        int t = 0,myn = 0;
+        base = 0;
+        int tst = input.size();
+        for(int i = 0; i < tst; i++){
+            int value = input[i];
+            myn += value/2;
+            // 使用固定轮转法确定比赛
+            allocat(base,value);
+            base += value;
+        }
+        tight_col = 0;
+        if(coln < 3) tight_col = 1;
+        if(coln == 3 && base == 8) tight_col = 1;
+        cntn = base;
+        int sum = 0;
+        for(int i = 0; i < len; i++) sum += ANS[i].size();
+        tmpn = min(coln,myn);
+        rown = sum / tmpn + !!(sum % tmpn);
+        TMP = !!((cntn-1) % coln) * rown;
+        bool myflag = 0;
+        for( EPS = 2;; EPS++){
+            // 逐步放宽EPS值的限制
+            memset(conf,0,sizeof(conf));
+            flag = 0;
+            init();
+            if(!myflag) best = 10000;
+            dfs(0,0,0);
+            if(myflag) break;
+            if(best != 10000) {
+                if(flag || best == TMP) break;
+                else myflag = 1;
+            }
+        }
+        __ans.resize(rown);
+        for(int i = 0; i < rown; i++){
+            for(int j = 0; j < coln; j++){
+                __ans[i].push_back(last_ans[i][j]);
+            }
+        }
+        return 1;
+    }
 };
